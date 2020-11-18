973a60933ed45753551eb88febc12923ca26fd25
Terry Bolton-Williams <55043494+TerrehDactyl@users.noreply.github.com>
2020-11-18 00:37:31 -0700

    Add files via upload

---
+++ icon.c
@@ -0,0 +1,12 @@
+#include "gtktemplate.h"
+
+// compile with gcc -Wall -g icon.c -o icon `pkg-config --cflags --libs gtk+-3.0`
+
+int main( int argc, char *argv[])
+{
+	gtk_init(&argc, &argv);
+
+	GtkWidget *window = createwindow("icon", GTK_WIN_POS_CENTER, "test.png");
+
+	show_and_destroy(window);
+}
