f7548f0df6ce8e3bea6ebc6684c695f69612a520
Terry Bolton-Williams <55043494+TerrehDactyl@users.noreply.github.com>
2020-11-18 00:17:54 -0700

    Update gtkwindow.c
    added icon to window function

--- gtkwindow.c
+++ gtkwindow.c
@@ -1,8 +1,8 @@
 #include "gtktemplate.h"
-//compile with gcc -Wall -g gtkwindow.c -o gtkwindow `pkg-config --cflags --libs gtk+-3.0`
-int main(int argc, char *argv[])
+// compile with gcc -Wall -g icon.c -o icon `pkg-config --cflags --libs gtk+-3.0`
+int main( int argc, char *argv[])
 {
-	gtk_init(&argc, &argv); //initializes GTK
-	GtkWidget *window = createwindow("new window", GTK_WIN_POS_CENTER); //creates a window with the title new window and positions it in the center of the screen
-	show_and_destroy(window); //creates the callback to destory the window, shows the window, and runs the gtk main loop
+	gtk_init(&argc, &argv);
+	GtkWidget *window = createwindow("icon", GTK_WIN_POS_CENTER, "test.png");
+	show_and_destroy(window);
 }
