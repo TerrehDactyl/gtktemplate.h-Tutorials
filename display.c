a5fd007f91645974cd7001d7a5d3a9890e619c13
Terry Bolton-Williams <55043494+TerrehDactyl@users.noreply.github.com>
2020-11-18 00:35:17 -0700

    Update display.c

--- display.c
+++ display.c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
 	gtk_init(&argc, &argv); //initializes GTK
 
-	GtkWidget *window = createwindow("new window", GTK_WIN_POS_CENTER); //creates a window with the title new window and positions it in the center of the screen
+	GtkWidget *window = createwindow("new window", GTK_WIN_POS_CENTER, "test.png"); //creates a window with the title new window and positions it in the center of the screen
 	GtkWidget *display = create_text_display(TRUE, 150, 40); //sets the display widget as a text display 
 	GtkTextBuffer *buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (display)); //sets thebuffer widget to the text gwidget.buffer of display
 	gtk_text_buffer_set_text (buffer, "enter some text", -1); //displays input.num1 
