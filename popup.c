eb2a08380d83a93d27b89bb839d6235529e7598d
Terry Bolton-Williams <55043494+TerrehDactyl@users.noreply.github.com>
2020-11-18 00:40:40 -0700

    Update popup.c

--- popup.c
+++ popup.c
@@ -3,13 +3,11 @@
 
 int main(int argc, char *argv [])
 {
-
 gtk_init(&argc, &argv); //starting gtk 
 
-GtkWidget *window = createwindow("main window",GTK_WIN_POS_CENTER); //popups can be done with createwindow as well 
-GtkWidget *popup = create_custom_window("popup", GTK_WINDOW_TOPLEVEL, GTK_WIN_POS_CENTER, 480,640);
+GtkWidget *window = createwindow("main window",GTK_WIN_POS_CENTER, "test.png"); //popups can be done with createwindow as well 
+GtkWidget *popup = create_custom_window("popup", GTK_WINDOW_TOPLEVEL, GTK_WIN_POS_CENTER, "test.png",480,640);
 gtk_widget_show_all(popup); //you can use show and destroy for this but it will make the windows
                             //tied together so if you close one they both close 
 show_and_destroy(window); //shows all widgets, connects the callback for the window and starts gtkmain
-
 }
